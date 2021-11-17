07f876afc3255447208e8bd706195b9d3b1e7c1a
MBOn20 <mel.bildhoff.20@lehre.mosbach.dhbw.de>
2021-11-17 13:25:54 +0100

    login und posts seperat

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::resource('posts', PostController::class);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
